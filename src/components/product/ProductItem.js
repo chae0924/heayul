345469d2b812bb4d0f0e805eb44db91b2c71dfa1
4thdraw <4thdraw2@gmail.com>
2024-11-20 15:15:34 +0900

    svgicon 스타일컴포넌트제작

--- src/components/product/ProductItem.js
+++ src/components/product/ProductItem.js
@@ -1,4 +1,5 @@
 import React, {useEffect} from 'react'
+import { Wishheart , BookmarkBt, CommentBt , RateBt , Syoutube, Sgit, Sinstar , Skakao,  LabelR, LabelC,  LabelPw , Viewicon, Carticon, Wishicon, Bookicon  } from '../common/util/_icon'
 import prditem from './ProductItem.module.scss' 
 
 export default function ProductItem({info}) {
@@ -11,10 +12,26 @@ export default function ProductItem({info}) {
               <div className="product-info">
                 <h3>{info.name}</h3>
                 <p>{info.description}</p>
-                <div className="price">
+                <div className="price d-flex">
                   <span className="original">{info.originalPrice.toLocaleString()}원</span>
                   <span className="discount">{info.discountPrice.toLocaleString()}원</span>
                 </div>
+                ------------------               
+                <Wishheart ></Wishheart>
+                <BookmarkBt></BookmarkBt>
+                <CommentBt></CommentBt>
+                <RateBt></RateBt>
+                <Syoutube></Syoutube>
+                <Sinstar></Sinstar>
+                <Skakao></Skakao>
+                <Sgit></Sgit>
+                <LabelR></LabelR>
+                <LabelC></LabelC>
+                <LabelPw></LabelPw> 
+                <Viewicon></Viewicon>
+                <Carticon></Carticon>
+                <Wishicon></Wishicon>
+                <Bookicon></Bookicon>
               </div>
     </div>
   )
