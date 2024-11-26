3c0b74dc061f092bc09fec8ad9efc4ed508d39be
dmin0308 <sonja4485@gmail.com>
2024-11-26 14:41:13 +0900

    1.0.0ver_ndm

--- src/App.js
+++ src/App.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-=======
 import React, { useEffect, useRef, useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
@@ -123,4 +96,3 @@ export default function App() {
     </div>
   )
 }
->>>>>>> 4ca1762a773aa944d7c8d47ff244fd4d15125c92
