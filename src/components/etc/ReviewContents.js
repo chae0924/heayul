610c536e0a9125091a5914a0d795f3d80725091e
kaneira2178 <kaneira2178@naver.com>
2024-12-03 17:38:24 +0900

    소비자 리뷰 수정

--- src/components/etc/ReviewContents.js
+++ src/components/etc/ReviewContents.js
@@ -1,13 +1,15 @@
 // ReviewContents 컴포넌트
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide} from 'swiper/react';
 import { Pagination, Autoplay } from "swiper/modules";
-import CustomerReview from "./CustomerReviews";
 import rs from "./reviewContents.module.scss";
+import reviews from "../../data/review.json"; 
+
 
 export default function ReviewContents() {
   return (
     <div className={`${rs.reviewContents} mt120 mb120`}>
       <h2 className=".kr_h2 mb26 mw">소비자의 생생한 이용후기</h2>
+      
       <Swiper
         loop={false}
         modules={[Pagination, Autoplay]}
@@ -23,31 +25,45 @@ export default function ReviewContents() {
           },
           1300: {
             slidesPerView: 4.6,
-            spaceBetween: 0,
+            spaceBetween: 100,
           },
         }}
       >
-        <SwiperSlide className={rs.swiperSlide}>
-          <CustomerReview />
+     
+     {reviews.map((item, index) =>   <SwiperSlide className={rs.className}>
+          <div className={rs.wrapper}>
+            {/* 메인 이미지 */}
+            <img
+              src={item.image_url}
+              className="img-fluid"
+              style={{ borderRadius: "8px" }}
+              alt={item.image_alt}
+            />
+          </div>
+
+          <div className="d-flex">
+            {/* 리뷰 내용 */}
+            <div className={rs.contentsbox}>
+              <div className={`${rs.text} kr-body`}>
+                {item.userReview}
+              </div>
+              <div className="kr-h5 ms-2">{item.userId}</div>
+            </div>
+
+            {/* 작성자 이미지 및 제품 이름 */}
+            <div>
+              <img
+                src={item.userImg}
+                className={`img-fluid ${rs.smallpicture}`}
+                alt={`${item.userId}의 리뷰 이미지`}
+              />
+              <p className={`${rs.productName} kr-body`}>{item.name}</p>
+            </div>
+          </div>
         </SwiperSlide>
-        <SwiperSlide className={rs.swiperSlide}>
-          <CustomerReview />
-        </SwiperSlide>
-        <SwiperSlide className={rs.swiperSlide}>
-          <CustomerReview />
-        </SwiperSlide>
-        <SwiperSlide className={rs.swiperSlide}>
-          <CustomerReview />
-        </SwiperSlide>
-        <SwiperSlide className={rs.swiperSlide}>
-          <CustomerReview />
-        </SwiperSlide>
-        <SwiperSlide className={rs.swiperSlide}>
-          <CustomerReview />
-        </SwiperSlide>
-        <SwiperSlide className={rs.swiperSlide}>
-          <CustomerReview />
-        </SwiperSlide>
+      )}
+      
+        
       </Swiper>
     </div>
   );
