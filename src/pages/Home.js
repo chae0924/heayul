1e65296be5fc2f3555186209b43190e9c906a750
dmin0308 <sonja4485@gmail.com>
2024-12-03 12:16:42 +0900

    슈퍼베이스 연동

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,9 +20,6 @@ export default function Home({ addToCart }) {
       <SwiperBanner id="mainSwiper"></SwiperBanner>
       <Svgicon id='mainSvg'></Svgicon> 
       
-      {/* 컴포넌트 아이디, 스타일, 상품데이터 필더조건, 더보기의 링크값 */}
-      <ProductThumbSet   id="newProduct"   style="col-6 col-lg-3" filterNV="badges|N" to="/product/newArrival" className='productThumbSet mw mb160'  addToCart={ addToCart } ></ProductThumbSet>
-
       {/* 컴포넌트 아이디, 스타일, 상품데이터 필더조건, 더보기의 링크값ㅋ */}
       <ProductThumbSet   id="newProduct"   style="" filterNV="badges|N" to="/product/newArrival" className='productThumbSet mw mb160'  addToCart={ addToCart } ></ProductThumbSet>
       
