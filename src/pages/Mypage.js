c35f2087e751542f85d976eb64a04e947b24e768
dmin0308 <sonja4485@gmail.com>
2024-12-04 11:24:39 +0900

    회원가입_로그인 마무리

---
+++ src/pages/Mypage.js
@@ -0,0 +1,72 @@
+import React from 'react'
+
+export default function Mypage() {
+  return (
+    <div className="css-u71x2d eug5r8l4">
+      <div className="css-5weut3 eug5r8l3">
+        <div className="css-1l2tkkh eug5r8l2">
+          <div className="pc css-3rbu7q epggjnz0">
+            <div className="top-info-section">
+              <div className="pc css-jagjhu e1w7rsa02">
+                <div className="grade-name">
+                  <div
+                    className="lottie-text"
+                    style={{
+                      minWidth: '76px',
+                      width: '76px',
+                      height: '28px',
+                      marginTop: '3px',
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlnsXlink="http://www.w3.org/1999/xlink"
+                      viewBox="0 0 75 28"
+                      width="75"
+                      height="28"
+                      preserveAspectRatio="xMidYMid slice"
+                      style={{
+                        width: '100%',
+                        height: '100%',
+                        transform: 'translate3d(0px, 0px, 0px)',
+                        contentVisibility: 'visible',
+                      }}
+                    >
+                      <defs>
+                        <clipPath id="__lottie_element_27">
+                          <rect width="75" height="28" x="0" y="0" />
+                        </clipPath>
+                      </defs>
+                      <image
+                        xlinkHref="data:image/png;base64,[BASE64_DATA]"
+                        alt="Grade Name"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="profile-section">
+              <div className="profile">
+                <div className="profile-img">
+                  <img
+                    src="https://via.placeholder.com/100"
+                    alt="User Profile"
+                  />
+                </div>
+                <div className="profile-details">
+                  <h2 className="username">User Name</h2>
+                  <p className="user-email">user@example.com</p>
+                </div>
+              </div>
+            </div>
+            <div className="membership-status">
+              <h3>Membership Level</h3>
+              <p>Gold Member</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
