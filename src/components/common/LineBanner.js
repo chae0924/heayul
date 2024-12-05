14cfa63cf9c741e5cf4c570987ad730b53933f63
kaneira2178 <kaneira2178@naver.com>
2024-12-05 12:45:20 +0900

    풋터 작업끝

--- src/components/common/LineBanner.js
+++ src/components/common/LineBanner.js
@@ -1,25 +1,37 @@
 // LineBanner.jsx
 import lb from "./lineBanner.module.scss";
 
-import Hsvg from '../../assets/svg/H.svg';  // 이미지 경로 import
-import Esvg from '../../assets/svg/E.svg';  // 이미지 경로 import
-import Ysvg from '../../assets/svg/Y.svg';  // 이미지 경로 import
-import Usvg from '../../assets/svg/U.svg';  // 이미지 경로 import
-import Lsvg from '../../assets/svg/L.svg';  // 이미지 경로 import
+import hsvg from '../../assets/svg/h.svg';  // 이미지 경로 import
+import esvg from '../../assets/svg/e.svg';  // 이미지 경로 import
+import ysvg from '../../assets/svg/y.svg';  // 이미지 경로 import
+import usvg from '../../assets/svg/u.svg';  // 이미지 경로 import
+import lsvg from '../../assets/svg/l.svg';  // 이미지 경로 import
+import download from '../../assets/svg/download.svg';  // 이미지 경로 import
 
 export default function Linebanner() {
   return (
     <div>
 
       <div className={lb.greenBanner}>
-        <div className={`d-flex ${lb.movingText} mw jusitfy-content-between `}>
+      <div className={`d-flex ${lb.movingTextBox} mw`}>
 
-            <div style={{ backgroundImage: `url(${Hsvg})` }} className={`${lb.h}`}></div>
-            <div style={{ backgroundImage: `url(${Esvg})` }} className={`${lb.e}`}></div>
-            <div style={{ backgroundImage: `url(${Ysvg})` }} className={`${lb.y}`}></div>
-            <div style={{ backgroundImage: `url(${Usvg})` }} className={`${lb.u}`}></div>
-            <div style={{ backgroundImage: `url(${Lsvg})` }} className={`${lb.l}`}></div>
-     
+            <div style={{ backgroundImage: `url(${hsvg})` }} className={`${lb.h}`}></div>
+            <div style={{ backgroundImage: `url(${esvg})` }} className={`${lb.e}`}></div>
+            <div style={{ backgroundImage: `url(${ysvg})` }} className={`${lb.y}`}></div>
+            <div style={{ backgroundImage: `url(${usvg})` }} className={`${lb.u}`}></div>
+            <div style={{ backgroundImage: `url(${lsvg})` }} className={`${lb.l}`}></div>
+          
+
+            <div className={`${lb.TextBox} d-flex gap-4`}>
+              <div className={`${lb.text}`}>
+                해율의 서비스를 모바일에서 편하게 즐겨보세요!
+              </div>
+              <a href="/path/to/download"download                  
+                  style={{ backgroundImage: `url(${download})` }} 
+                  className={`${lb.downbtn}`}>
+              </a>
+            </div>
+
         </div>
       </div>
     </div>
